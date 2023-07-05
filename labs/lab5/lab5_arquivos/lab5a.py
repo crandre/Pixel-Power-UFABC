d6b66e24d8ce32c6e556405cd9d1758eeea5a28f
giovannaandrade <giovanna.7@hotmail.com>
2023-07-05 20:22:08 -0300

    new file:   labs/lab5/lab5_arquivos.zip 	new file:   labs/lab5/lab5_arquivos/lab5a.py 	new file:   labs/lab5/lab5_arquivos/lab5b.py 	new file:   labs/lab5/lab5_arquivos/lab5c.py 	new file:   labs/lab5/lab5_arquivos/vtest.avi

---
+++ labs/lab5/lab5_arquivos/lab5a.py
@@ -0,0 +1,34 @@
+
+# import the opencv module
+import cv2 as cv
+
+# capturing video
+capture = cv.VideoCapture("./labs/lab5/lab5_arquivos/vtest.avi")
+
+backSub = cv.createBackgroundSubtractorMOG2()
+
+#backSub = cv.createBackgroundSubtractorKNN()
+
+if not capture.isOpened():
+ print('Unable to open: ' + args.input)
+ exit(0)
+
+while True:
+ ret, frame = capture.read()
+ if frame is None:
+   break
+ 
+ fgMask = backSub.apply(frame)
+ 
+ 
+ cv.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
+ cv.putText(frame, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
+ cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+ 
+ 
+ cv.imshow('Frame', frame)
+ cv.imshow('FG Mask', fgMask)
+ 
+ keyboard = cv.waitKey(30)
+ if keyboard == 'q' or keyboard == 27:
+   break
